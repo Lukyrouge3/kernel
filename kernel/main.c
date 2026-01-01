3c456a23c3cd45e00dfd798dcb367c1838d2c7a1
tienshi <sebspubemail@gmail.com>
2026-01-01 23:30:14 +0100

    Made changes according to pull request

--- kernel/main.c
+++ kernel/main.c
@@ -8,37 +8,43 @@ void _start(void) __attribute__((section(".init")));
 
 #define TEXT_COLOR 0x0F
 
+#define VGA_CRT_CTRL_ADDR 0x3D4
+#define VGA_CRT_CTRL_DATA 0x3D5
+
+#define  VGA_CURSOR_LOC_HIGH 0x0F
+#define  VGA_CURSOR_LOC_LOW 0x0E
+
 static volatile uint16_t* const VGA = (uint16_t*)0xB8000;
 static uint16_t vga_entry(char c, uint8_t color) {
     return (uint16_t)c | ((uint16_t)color << 8);
 }
 
-static inline void outb(uint16_t port, uint8_t val) {
+inline void outb(uint16_t port, uint8_t val) {
     __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
 }
 
-static inline uint8_t inb(uint16_t port) {
+inline uint8_t inb(uint16_t port) {
     uint8_t ret;
     __asm__ volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
     return ret;
 }
 
-static uint16_t get_cursor_pos(void) {
-    outb(0x3D4, 0x0F);
-    uint16_t pos = inb(0x3D5);
-    outb(0x3D4, 0x0E);
-    pos |= (uint16_t)inb(0x3D5) << 8;
+uint16_t get_cursor_pos(void) {  
+    outb(VGA_CRT_CTRL_ADDR, VGA_CURSOR_LOC_HIGH);
+    uint16_t pos = inb(VGA_CRT_CTRL_DATA);
+    outb(VGA_CRT_CTRL_ADDR, VGA_CURSOR_LOC_LOW);
+    pos |= (uint16_t)inb(VGA_CRT_CTRL_DATA) << 8;
     return pos;
 }
 
 void set_cursor_pos(uint16_t pos) {
-    outb(0x3D4, 0x0F);
-    outb(0x3D5, (uint8_t)(pos & 0xFF));
-    outb(0x3D4, 0x0E);
-    outb(0x3D5, (uint8_t)((pos >> 8) & 0xFF));
+    outb(VGA_CRT_CTRL_ADDR, VGA_CURSOR_LOC_HIGH);
+    outb(VGA_CRT_CTRL_DATA, (uint8_t)(pos & 0xFF));
+    outb(VGA_CRT_CTRL_ADDR, VGA_CURSOR_LOC_LOW);
+    outb(VGA_CRT_CTRL_DATA, (uint8_t)((pos >> 8) & 0xFF));
 }
 
-void    clearScreen(void)
+void    clear_screen(void)
 {
     for (int i = 0; i < VGA_ENTRIES; i++)
     {
@@ -84,9 +90,8 @@ void    scroll(void)
 }
 
 void _start(void) {
-    clearScreen();
+    clear_screen();
     kprint("Hello from kernel!\nThis is a new line that updates the cursor's position accordingly.");
-    kprint("This is a test to see if with a newline from kprint and a long sentence the nl\nworks");
     scroll();
     while(1)
         ;
