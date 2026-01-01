b5304a286dbdaeef4a94bb6dcaa663e54be1c886
tienshi <sebspubemail@gmail.com>
2026-01-01 20:08:24 +0100

    changed printing function to accept nl character. added scrolling command

--- kernel/main.c
+++ kernel/main.c
@@ -2,33 +2,92 @@
 
 void _start(void) __attribute__((section(".init")));
 
-#define VGA_HEIGHT 80
-#define VGA_WIDTH 25
+#define VGA_HEIGHT 25
+#define VGA_WIDTH 80
+#define VGA_ENTRIES (VGA_WIDTH * VGA_HEIGHT)
+
+#define TEXT_COLOR 0x0F
 
 static volatile uint16_t* const VGA = (uint16_t*)0xB8000;
 static uint16_t vga_entry(char c, uint8_t color) {
     return (uint16_t)c | ((uint16_t)color << 8);
 }
 
-void    clearScreen(void)
-{
-    for (int i = 0; i < VGA_HEIGHT * VGA_WIDTH; i++)
-    {
-        VGA[i] = 0x0;
-    }
+static inline void outb(uint16_t port, uint8_t val) {
+    __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
 }
 
-void    kprint(const char* string, uint8_t color)
+static inline uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    __asm__ volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+static uint16_t get_cursor_pos(void) {
+    outb(0x3D4, 0x0F);
+    uint16_t pos = inb(0x3D5);
+    outb(0x3D4, 0x0E);
+    pos |= (uint16_t)inb(0x3D5) << 8;
+    return pos;
+}
+
+void set_cursor_pos(uint16_t pos) {
+    outb(0x3D4, 0x0F);
+    outb(0x3D5, (uint8_t)(pos & 0xFF));
+    outb(0x3D4, 0x0E);
+    outb(0x3D5, (uint8_t)((pos >> 8) & 0xFF));
+}
+
+void    clearScreen(void)
 {
-    for (int i = 0; string[i]; i++) {
-        VGA[i] = vga_entry(string[i], color); // white on black
+    for (int i = 0; i < VGA_ENTRIES; i++)
+    {
+        VGA[i] = vga_entry(' ', (uint8_t)TEXT_COLOR);
     }
+    set_cursor_pos(0);
+}
+
+void    kprint(const char* string)
+{
+    uint16_t pos = get_cursor_pos();
+
+    for (int i = 0; string[i] && pos < VGA_ENTRIES; i++) {
+        char c = string[i];
+
+        if (c == '\n') {
+            pos = (pos / VGA_WIDTH + 1) * VGA_WIDTH;
+            continue;
+        }
+
+        VGA[pos++] = vga_entry(c, (uint8_t)TEXT_COLOR);
+    }
+
+    if (pos >= VGA_ENTRIES)
+        pos = VGA_ENTRIES - 1;
+    set_cursor_pos(pos);
+}
+
+void    scroll(void)
+{
+    uint8_t pos = get_cursor_pos();
+
+    for (int i = 0; i < pos; i++)
+    {
+        if (i + VGA_WIDTH > pos)
+        {
+            VGA[i] = vga_entry(' ', TEXT_COLOR);
+            continue;
+        }
+        VGA[i] = vga_entry(VGA[i + VGA_WIDTH], TEXT_COLOR);
+    }
+    set_cursor_pos(pos - VGA_WIDTH);
 }
 
 void _start(void) {
-    const char* s = "hello from c in pmode";
     clearScreen();
-    kprint(s, 0x0F);
-
-    for (;;) {  }
+    kprint("Hello from kernel!\nThis is a new line that updates the cursor's position accordingly.");
+    kprint("This is a test to see if with a newline from kprint and a long sentence the nl\nworks");
+    scroll();
+    while(1)
+        ;
 }
